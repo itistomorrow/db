0a2839b34142a1018efab06707d0154a221a91bd
dadol2 <dadol2@nate.com>
2014-12-19 18:02:30 +0900

    ㅇ

--- student_main.php
+++ student_main.php
@@ -6,12 +6,8 @@
 <frame name="top" frameborder="0" src="stu_menu.html" scrolling="no" noresize>
 <frame name="bottom" frameborder="0" bordercolor="black" border="1" src="stu_class_search.html">
 <noframes>
-<body><<<<<<< HEAD
-
-=======
-</br>
+<body>
 <p>¿ÀÀ×</p>
->>>>>>> 306b5ebe0293cd7a5be6bcd06f725cb25b993c1f
 </body>
 </noframes>
 </frameset>
