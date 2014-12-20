be37120b4e85e06957c28d006f70257d3417c3dd
dadol2 <dadol2@nate.com>
2014-12-20 18:38:40 +0900

    change index

--- index.php
+++ index.php
@@ -25,34 +25,61 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		<style>
+			.syntaxhighlighter.bootstrap {
+				padding: 9px 14px;
+				margin-bottom: 14px;
+				background: #f7f7f9;
+				border: 1px solid #e1e1e8;
+				border-radius: 4px;
+			}
+			h1, h2, h3, h4, h5, h6, p, a, div, code {
+				font-family: 'Malgun Gothic';
+			}
+			.bs-example:after {
+				content: "예제";
+			}
+			body
+			{
+				padding-top: 80px;
+			}
+		</style>
 	</head>
 
 	<body>
 		<div>
-			<header>
-				<h2 class="lead" align="center"> 2013년도 2학기 데이터베이스 모의 수강 시스템 </h2>
+			<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+			
+				<div class="container">
+					
+					<p class="navbar-brand text-center"> 2013년도 2학기 데이터베이스 모의 수강 시스템 </p>
+					
+
+				</div>				
 			</header>
+				
+		
 			<div class="row">
 
-				<div class="container col-md-4 col-md-offset-4 ">
-					<div class="jumbotron" >
+				<div class="container col-md-5 col-md-offset-1 ">
+					<div class="well well-lg">
 
-						<form role="form" NAME=LOGIN METHOD=POST ACTION="login.php">
+						<form role="form" NAME="LOGIN" METHOD="POST" ACTION="login.php">
 
 							<div class="form-group">
 								<label for="exampleInputID1">ID</label>
-								<input type="id" class="form-control" placeholder="Enter ID" name="id">
+								<input type="id" class="form-control input-lg" placeholder="Enter ID" name="id">
 							</div>
 							<div class="form-group">
 								<label for="exampleInputPassword1">Password</label>
-								<input type="password" class="form-control" placeholder="Enter Password" name ="passwd">
+								<input type="password" class="form-control input-lg" placeholder="Enter Password" name ="passwd">
 							</div>
 							<div class="form-group">
-								<div class=".col-xs-6 .col-md-4">
-									<button type="submit" class="btn btn-default" name="btn1">
+								
+									<button type="submit" class="btn btn-primary btn-lg" name="btn1">
 										Log in
 									</button>
-								</div>
+								
 							</div>
 						</form>
 					</div>
